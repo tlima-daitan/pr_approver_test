efbeb2e6a4bcc56ae6547c2f4345afa288c7d1b0
fmontagnani-daitan <fmontagnani@daitan.com>
2021-09-16 09:37:30 -0300

    added action

---
+++ Readme.md
@@ -0,0 +1,6 @@
+PR Auto Approver
+
+A Github Action to automatically approve PRs according to a given label.
+
+Usage
+
---
+++ index.js
@@ -0,0 +1,22 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+
+const main = async () => {
+  const token = core.getInput('token');
+  const label = core.getInput('target_label');
+  const octokit = github.getOctokit(token);
+
+  const { pull_request } = context.payload;
+  const { number, labels } = pull_request;
+
+  if (labels.includes(label)) {
+    await octokit.rest.pulls.submitReview({
+      ...github.context.repo,
+      pull_number: number,
+      event: 'APPROVE'
+    })
+  }
+
+};
+
+main().catch(err => core.setFailed(err.message));
