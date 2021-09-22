4ed76dccac234b95ecb08d11c9d87310e37a8497
Thiago L. Lima <tlima@daitan.com>
2021-09-22 17:10:56 -0300

    Removing invalid array, switching to string

--- dist/index.js
+++ dist/index.js
@@ -6329,23 +6329,25 @@ const core = __nccwpck_require__(186);
 
 const main = async () => {
   const token = core.getInput('token');
-  const approvalLabels = core.getInput('label-list');
-  const exclusionLabels = core.getInput('label-exclusion-list');
+  const approvalLabelsString = core.getInput('label-list');
+  const exclusionLabelsString = core.getInput('label-exclusion-list');
   const octokit = github.getOctokit(token);
 
   const { pull_request } = github.context.payload;
   const { number, labels } = pull_request;
 
-  const labelsList = labels.map(label => label.name);
+  const prLabels = labels.map(label => label.name);
+  const approvalLabels = approvalLabelsString.split(',');
+  const exclusionLabels = exclusionLabelsString && exclusionLabelsString.split(',');
 
   console.log('\n\n### ~~^~~ ~~^~~ ~~^~~ ~~^~~ ~~^~~ ~~^~~ ~~^~~ ###');
   console.log(approvalLabels);
   console.log(exclusionLabels);
   console.log('### ~~@~~ ~~@~~ ~~@~~ ~~@~~ ~~@~~ ~~@~~ ~~@~~ ###\n\n');
 
-  if (Array.isArray(exclusionLabels) && exclusionLabels.filter(exclusionLabel => labelsList.includes(exclusionLabel)).length > 0) return;
+  if (Array.isArray(exclusionLabels) && exclusionLabels.filter(exclusionLabel => prLabels.includes(exclusionLabel)).length > 0) return;
 
-  if (approvalLabels.filter(approvalLabel => labelsList.includes(approvalLabel))) {
+  if (approvalLabels.filter(approvalLabel => prLabels.includes(approvalLabel))) {
     await octokit.rest.pulls.createReview({
       ...github.context.repo,
       pull_number: number,
